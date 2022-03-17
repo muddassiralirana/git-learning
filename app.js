ac03acb123eff9894bb5b936e58ba50004f2a148
muddassirAliRana <muddassiralirana786@gmail.com>
2022-03-17 23:07:53 +0500

    add work

---
+++ app.js
@@ -0,0 +1,83 @@
+// alert("my name is rana")
+
+
+
+
+
+// function popup(){
+//     alert("my name is rana")
+// }
+
+// popup()
+
+// window.alert("my name is sharjeel")
+
+
+
+// var name= "sharjeel";
+
+//  var  variable name=  value
+// var fName="shujat";
+
+// var number=5
+
+// number =number+5
+
+// document.write(number)
+// console.log(number)
+
+// alert(number)
+
+// var no1=10
+// var no2=20
+// console.log(no1)
+
+// // no1=+no2
+
+// var no3= no1 +no2
+
+// console.log(no3)
+
+// var num1=5
+
+// var num2= num1+10
+// var num2= num1*10
+// var num2= num1/10
+// var num2= num1-10
+
+// console.log(num2)
+
+
+// var num = 5;
+
+// var newNum= ++num
+// var newNum= num++
+// var newNum= --num
+// var newNum= num--
+
+// console.log(newNum)
+
+// var solve=(10*(5))/2+34-5*4
+
+// console.log(solve)
+
+// var name= "sharjeel"
+// var fName="shuja bhai"
+
+// console.log(name+" " +fName)
+
+// var fullName=name + " " + fName
+
+// console.log(fullName)
+
+
+
+var num =prompt("enter a num")
+
+document.write(num)
+
+
+
+
+
+
